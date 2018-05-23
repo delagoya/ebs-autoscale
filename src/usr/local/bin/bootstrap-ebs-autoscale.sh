ef9b1f8f569dcd50b5ca3b0027c0e0aed136836c
Angel Pizarro <delagoya@gmail.com>
2018-05-22 21:26:27 -0400

    bunch of bug fixes

--- src/usr/local/bin/bootstrap-ebs-autoscale.sh
+++ src/usr/local/bin/bootstrap-ebs-autoscale.sh
@@ -1,20 +1,37 @@
-if [ "$#" -ne "4" ]; then
-  echo "USAGE: $0 <DEVICE> <VOLUME GROUP NAME> <LOGICAL VOLUME NAME> <MOUNT POINT>"
+# if ebs-autoscale exe exists, skip bootstrap
+if [ -e /usr/local/bin/ebs-autoscale ]; then
+  exit 0
+fi
+
+function printUsage() {
+  #statements
+  echo "USAGE: $0 <VOLUME GROUP NAME> <LOGICAL VOLUME NAME> <MOUNT POINT>"
+}
+
+if [ "$#" -ne "3" ]; then
+  printUsage
   exit 1
 fi
 
-DV=$1
-VG=$2
-LV=$3
-MP=$4
+# If the ebs-autoscale conf file exists, then we should exit
+if [ -b "$2" ]; then
+  echo "LOGICAL VOLUME exists $2."
+  printUsage
+  exit 1
+fi
+
+VG=$1
+LV=$2
+MP=$3
 AZ=$(curl -s  http://169.254.169.254/latest/meta-data/placement/availability-zone/)
-RG=$(echo $Z | sed -e 's/[a-z]$//')
+RG=$(echo ${AZ} | sed -e 's/[a-z]$//')
 IN=$(curl -s  http://169.254.169.254/latest/meta-data/instance-id)
 
-# if docker-ebs-autoscale exe exists, skip bootstrap
-if [ -e /usr/local/bin/docker-ebs-autoscale ]; then
-  exit 0
-fi
+# Get the next device ID
+A=({a..z})
+N=$(ls /dev/xvd* | grep -v -E '[0-9]$' | wc -l)
+DV="/dev/xvd${A[$N]}"
+
 
 # Create and attache the EBS Volume, also set it to delete on instance terminate
 V=$(aws ec2 create-volume --region ${RG} --availability-zone ${AZ} --volume-type gp2 --size 10 --encrypted --query "VolumeId" | sed 's/\"//g' )
@@ -47,10 +64,27 @@ lvcreate -l $E  -n ${LV} ${VG}
 
 #make the filesystem and mount it
 mkfs.ext4 /dev/${VG}/${LV}
-mkdir ${MP}
+if ! [ -d "${MP}" ]; then
+  mkdir -p ${MP}
+fi
 mount /dev/${VG}/${LV} ${MP}
 echo -e "/dev/${VG}/${LV}\t${MP}\text4\tdefaults\t0\t0" |  tee -a /etc/fstab
 
+# copy out the upstart template
+# cd /opt/ebs-autoscale/etc/init
+cd $(dirname $0)/../../../
+cp etc/init/ebs-autoscale.conf.template ebs-autoscale.conf
+sed -i -e "s#YOUR_DV#${DV}#" ebs-autoscale.conf
+sed -i -e "s#YOUR_VG#${VG}#" ebs-autoscale.conf
+sed -i -e "s#YOUR_LV#${LV}#" ebs-autoscale.conf
+sed -i -e "s#YOUR_MP#${MP}#" ebs-autoscale.conf
+cp ebs-autoscale.conf  /etc/init/ebs-autoscale.conf
+# copy logrotate conf
+cp etc/logrotate.d/ebs-autoscale /etc/logrotate.d/ebs-autoscale
+
+# copy exe
+cp usr/local/bin/ebs-autoscale /usr/local/bin/ebs-autoscale
+
 # Register the ebs-autoscale upstart conf and start the service
 initctl reload-configuration
-initctl restart ebs-autoscale
+initctl start ebs-autoscale
